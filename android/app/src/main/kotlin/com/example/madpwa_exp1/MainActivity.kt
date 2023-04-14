84ecf239adafacc0306b323dcfd299d28ef84a8a
Sanika Surve <89775023+sanikasurve@users.noreply.github.com>
2023-04-14 22:39:15 +0530

    madpwa exp 1

---
+++ android/app/src/main/kotlin/com/example/madpwa_exp1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.madpwa_exp1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+    home : HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Test App"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Demo Text"),
+          Icon(Icons.emoji_emotions_sharp),
+        ],
+
+      ),
+
+      floatingActionButton: Container(
+        alignment: Alignment.center,
+      child: FloatingActionButton(
+        child: const Icon(Icons.thumb_up),
+        onPressed: () {
+          print("Sanika is Testing");
+        },
+        backgroundColor: Colors.green,
+
+
+      ),
+      ),
+    );
+
+  }
+
+}
